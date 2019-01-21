b1dd0732ba37275d0beb3c72a28a7faa3b89ea02
Bryce Nicholls <bnicholls@ukcloud.com>
2019-01-21 09:16:18 +0000

    edited gitignore and added node.sh script

---
+++ node.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+yum -y update
+yum -y install epel-release
+
+
+cat <<EOF > /home/centos/.vimrc
+autocmd FileType yaml setlocal ai ts=2 sw=2 et
+filetype plugin indent on
+set nocompatible
+set relativenumber
+set cursorline
+set showmatch
+set incsearch
+set hlsearch
+set expandtab
+EOF
+
+cat <<EOF > /root/.vimrc
+autocmd FileType yaml setlocal ai ts=2 sw=2 et
+filetype plugin indent on
+set nocompatible
+set relativenumber
+set cursorline
+set showmatch
+set incsearch
+set hlsearch
+set expandtab
+EOF
